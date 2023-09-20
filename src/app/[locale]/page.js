1e9b684f6daea21d3bf3c3714c801a1efd3aa0b7
gedesurya125 <gedesurya125@gmail.com>
2023-09-20 11:10:43 +0800

    add next intl

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -9,7 +9,7 @@ export default function Home() {
         <div className={styles.description}>
           <Link
             href={{
-              pathname: "/about",
+              pathname: "/super/",
               hash: "#target-section",
               query: {
                 page: 1,
